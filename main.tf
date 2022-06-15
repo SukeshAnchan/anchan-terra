c67402f6f3a3fc734b7c4a2824b1e9ba8d7f4c9f
SukeshAnchan <33873499+SukeshAnchan@users.noreply.github.com>
2022-06-15 11:23:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,49 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-sg"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-068257025f72f470d"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+  user_data     = <<EOF
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "<h1> Hello bosch</h1>" | sudo tee /var/www/html/index.html
+EOF
+
+  tags = {
+    Name = "terraform-example"
+  }
+}
